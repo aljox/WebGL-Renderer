38551284390589d980c88cc97c3b4541f16f833b
Aljaz Bogataj <ab9224@student.uni-lj.si>
2019-08-19 21:34:50 +0100

    Manipulation with universal and per object uniforms

--- Src/UniformManagment/UniformArray.js
+++ Src/UniformManagment/UniformArray.js
@@ -1,16 +1,35 @@
 //TODO: implement per object uniforms
 class UniformArray {
-  constructor(universalUniform, perObjectComputedUniform, perObjectUniform){
+  constructor(universalUniform, perObjectUniform){
     this.universalUniform = universalUniform;
-    this.perObjectComputedUniform = perObjectComputedUniform;
     this.perObjectUniform = perObjectUniform;
   }
 
+  addUniversalUniform(uniform){
+    if(!uniform instanceof Uniform) throw Error("Passed parameter is not uniform!");
+    this.universalUniform.push(uniform);
+  }
+
+  addUniversalUniformArray(uniforms){
+    // Todo: Error Check
+
+    this.universalUniform.push(...uniforms);
+  }
+
+  addObjectUniform(uniform){
+    if(!uniform instanceof Uniform) throw Error("Passed parameter is not uniform!");
+    this.perObjectUniform.push(uniform);
+  }
+
+  addObjectUniformArray(uniforms){
+    // Todo: error Check
+
+    this.perObjectUniform.push(...uniforms);
+  }
+
   getUniversalUniform(){return this.universalUniform;}
-  getPerObjectComputedUniform(){return this.perObjectComputedUniform;}
   getPerObjectUniform(){return this.perObjectUniform;}
 
   getUniversalUniformElement(i){return this.universalUniform[i];}
-  getPerObjectComputedUniformElement(i){return this.perObjectComputedUniform[i];}
   getPerObjectUniformElement(i){return this.perObjectUniform[i];}
 }
