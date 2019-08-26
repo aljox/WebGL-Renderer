9afac953acd8b5b51a3c66b5146d162f2dbc8676
Aljaz Bogataj <ab9224@student.uni-lj.si>
2019-08-26 11:43:53 +0100

    New light class

--- Src/Object/Light/Light.js
+++ Src/Object/Light/Light.js
@@ -1,14 +1,25 @@
-class Light extends RenderModel {
-  constructor(model, colour, diffuseIntensity, specularIntensity, ambientIntensity){
-    RenderModel renderModel = RenderModel.buildFromModel(model);
+class Light extends RenderModel{
+  constructor(renderModel, colour, ambientIntensity, diffuseIntensity, specularIntensity){
     super(renderModel.getVertexArray());
 
     this.type = "Light";
+    this.material = null;
 
     this.colour = colour;
+    this.ambientIntensity = ambientIntensity;
     this.diffuseIntensity = diffuseIntensity;
     this.specularIntensity = specularIntensity;
-    this.ambientIntensity = ambientIntensity;
+  }
+
+  buildUniformLightShader() {
+    this.objectUniforms.push(new Uniform("u_colour", "3f", this.colour.toArray()));
+  }
+
+  buildUniformObjectShader(object) {
+    object.addUniform(new Uniform("light.s_lightColour", "3f", this.colour.toArray()));
+    object.addUniform(new Uniform("light.s_ambient", "1f", this.ambientIntensity));
+    object.addUniform(new Uniform("light.s_diffuse", "1f", this.diffuseIntensity));
+    object.addUniform(new Uniform("light.s_specular", "1f", this.specularIntensity));
   }
 
   setColour(colour){this.colour = colour;}
@@ -16,8 +27,8 @@ class Light extends RenderModel {
   setSpecularIntesinty(specularIntensity) {this.specularIntensity = specularIntensity;}
   setAmbientIntensity(ambientIntensity) {this.ambientIntensity = ambientIntensity;}
 
-  getColour(){this.colour;}
-  getDiffuseIntensity() {this.diffuseIntensity;}
-  getSpecularIntesinty() {this.specularIntensity;}
-  getAmbientIntensity() {this.ambientIntensity;}
+  getColour(){return this.colour;}
+  getDiffuseIntensity() {return this.diffuseIntensity;}
+  getSpecularIntesinty() {return this.specularIntensity;}
+  getAmbientIntensity() {return this.ambientIntensity;}
 }
