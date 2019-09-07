a3e6b9d29cf62cc589ac4e4022d14e602b7efb27
Aljaz Bogataj <ab9224@student.uni-lj.si>
2019-09-07 12:12:40 +0100

    Format code.

--- Src/Loaders/Loader.js
+++ Src/Loaders/Loader.js
@@ -15,31 +15,31 @@ class Loader {
     });
   }
 
-  setType(type){this.type = type;}
-  setUrl(url){this.url = url;}
-  setLoaded(loaded){this.loaded = loaded;}
-  setData(data){this.data = data;}
+  setType(type) {this.type = type;}
+  setUrl(url) {this.url = url;}
+  setLoaded(loaded) {this.loaded = loaded;}
+  setData(data) {this.data = data;}
 
-  getType(){return this.type;}
-  getUrl(){return this.url;}
-  getLoaded(){return this.loaded;}
-  getData(){return this.data;}
+  getType() {return this.type;}
+  getUrl() {return this.url;}
+  getLoaded() {return this.loaded;}
+  getData() {return this.data;}
 }
 
-class ModelLoader extends Loader{
-  constructor(url){
+class ModelLoader extends Loader {
+  constructor(url) {
     super("OBJ_MODEL", url);
   }
 }
 
-class VertexShaderLoader extends Loader{
-  constructor(url){
+class VertexShaderLoader extends Loader {
+  constructor(url) {
     super("VERTEX_SHADER", url);
   }
 }
 
-class FragmentShaderLoader extends Loader{
-  constructor(url){
+class FragmentShaderLoader extends Loader {
+  constructor(url) {
     super("FRAGMENT_SHADER", url);
   }
 }
