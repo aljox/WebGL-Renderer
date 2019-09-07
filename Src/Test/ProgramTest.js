121f4eafd77b403633c2d5161c64547ba52a59a0
Aljaz Bogataj <ab9224@student.uni-lj.si>
2019-09-07 12:09:33 +0100

    Removed unnecessery test.

--- Src/Test/ProgramTest.js
+++
@@ -1,7 +0,0 @@
-let shaderList = ["basicVertex.vertex", "basicFragment.fragment"];
-let modelList = [];
-
-window.onload = function(){
-  let rendereEngine = new RenderEngine(shaderList, modelList);
-  rendereEngine.initialise();
-}
