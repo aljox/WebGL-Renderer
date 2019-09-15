6d53fbf24fd6977cc0fbbbaf257058ba9366fbc3
Aljaz Bogataj <ab9224@student.uni-lj.si>
2019-09-15 11:26:29 +0100

    Added Random Integer Function

--- Src/Math/MathExt.js
+++ Src/Math/MathExt.js
@@ -8,4 +8,11 @@ class MathExt{
   static isPowerOf2(value) {
     return (value & (value - 1)) == 0;
   }
+
+  static randomInteger(min, max) {
+  // here rand is from min to (max+1)
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+  }
+
 }
