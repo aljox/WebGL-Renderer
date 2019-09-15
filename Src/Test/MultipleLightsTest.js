1b27e331c9d5d3e23ebe0273fa84d435e4213845
Aljaz Bogataj <ab9224@student.uni-lj.si>
2019-09-15 11:27:25 +0100

    Multiple object and pointLights generation

--- Src/Test/MultipleLightsTest.js
+++ Src/Test/MultipleLightsTest.js
@@ -24,25 +24,14 @@ function initialise(renderEngine) {
   let directionalLight1 = new DirectionalLight(new Vec3f(0.1, 0.1, -1.0), new Vec3f(1.0, 1.0, 1.0), 0.0, 0.0, 0.0);
   let directionalLights = [directionalLight1];
 
-  let pointLight1 = new PointLight(renderEngine.getRenderModel(2), new Vec3f(1.0, 1.0, 1.0), 0.2, 0.8, 0.5,
-                                                                    new Vec3f(5, 20, -120), 1.0, 0.0007, 0.00014);
-  let pointLight2 = new PointLight(renderEngine.getRenderModel(2), new Vec3f(1.0, 1.0, 1.0), 0.2, 0.8, 0.5,
-                                                                    new Vec3f(-15, 15, -190), 1.0, 0.0007, 0.00014);
-
-  let pointLights = [pointLight1, pointLight2];
-
-  // Initialise model
-  let model = renderEngine.getRenderModel(0);
-  model.setPosition(new Vec3f(-10, 15, -160));
-  model.setRotation(new Vec3f(10, 30, 0));
-  model.setScale(new Vec3f(12, 12, 12));
-  model.updateModelMatrix();
+  let pointLights = generatePointLights(renderEngine.getRenderModel(2), 9);
 
   // Texture for model
   let modelTexture = renderEngine.getTexture(0);
   modelTexture.generateMipMap("LINEAR_MIPMAP_LINEAR");
-  //modelTexture.setParameters("CLAMP_TO_EDGE", "LINEAR");
-  model.setTexture(modelTexture);
+
+    // Initialise model
+  let models = generateModels(renderEngine.getRenderModel(0), 15, renderEngine.getTexture(1));
 
   // Initialise ground
   let ground = renderEngine.getRenderModel(1);
@@ -61,25 +50,11 @@ function initialise(renderEngine) {
 
   // Renderer
   let renderer = new Renderer(1);
-  let objToRender = [model, ground];
+  let objToRender = models.concat([ground]);
   objToRender = objToRender.concat(pointLights);
 
-  let then = 0;
-  function renderScene(now) {
-    // Convert to seconds
-    now *= 0.001;
-    // Subtract the previous time from the current time
-    let deltaTime = now - then;
-    // Remember the current time for the next frame.
-    then = now;
 
-    // Every frame increase the rotation a little.
-    // let rotation = model.getRotation();
-    // rotation = rotation.getY();
-    // rotation -= 20.0 * deltaTime;
-    // model.setRotationY(rotation);
-    // model.updateModelMatrix();
-
+  function renderScene() {
     for(let obj of objToRender){
       obj.clearUniforms();
     }
@@ -103,17 +78,19 @@ function initialise(renderEngine) {
     }
 
     // Model uniforms
-    let modelViewMatrixModel = Matrix44f.mulArray([model.getModelMatrix(),
-                                      perspectiveCamera.getViewMatrixInverse()]);
-    let modelViewTransoformModel = new UniformMatrix("u_modelViewTransform", "4fv", modelViewMatrixModel);
-    model.addUniform(modelViewTransoformModel);
+    for(let model of models){
+      let modelViewMatrixModel = Matrix44f.mulArray([model.getModelMatrix(),
+                                        perspectiveCamera.getViewMatrixInverse()]);
+      let modelViewTransoformModel = new UniformMatrix("u_modelViewTransform", "4fv", modelViewMatrixModel);
+      model.addUniform(modelViewTransoformModel);
 
-    let normalMatrix = Matrix44f.transponse(Matrix44f.inverse(modelViewMatrixModel));
-    let normalMatrixUniform = new UniformMatrix("u_normalMatrix", "4fv", normalMatrix);
-    model.addUniform(normalMatrixUniform);
+      let normalMatrix = Matrix44f.transponse(Matrix44f.inverse(modelViewMatrixModel));
+      let normalMatrixUniform = new UniformMatrix("u_normalMatrix", "4fv", normalMatrix);
+      model.addUniform(normalMatrixUniform);
 
-    DirectionalLight.buildUniformObjectShader(model, directionalLights);
-    PointLight.buildUniformObjectShader(model, pointLights, perspectiveCamera);
+      DirectionalLight.buildUniformObjectShader(model, directionalLights);
+      PointLight.buildUniformObjectShader(model, pointLights, perspectiveCamera);
+    }
 
     // Ground uniforms
     let groundViewMatrixGround = Matrix44f.mulArray([ground.getModelMatrix(),
@@ -131,6 +108,36 @@ function initialise(renderEngine) {
     // Global uniform
     let projectionTransform = new UniformMatrix("u_projection", "4fv", perspectiveCamera.getProjectionMatrix());
     return new UniformArray([projectionTransform], []);
+  }
+
+  function generateModels(renderModel, num, modelTexture){
+      let models = [];
+      for(let i = 0; i < num; i++){
+        let model = renderModel.getNewInstance();
+        model.setPosition(new Vec3f(MathExt.randomInteger(-30, 30), MathExt.randomInteger(3, 30), MathExt.randomInteger(-200, -100)));
+        model.setRotation(new Vec3f(MathExt.randomInteger(-90, 90), MathExt.randomInteger(-90, 90), MathExt.randomInteger(-90, 90)));
+        model.setScale(new Vec3f(8, 8, 8));
+        model.updateModelMatrix();
+
+        model.setTexture(modelTexture);
+
+        models[i] = model;
+      }
+
+      return models;
+  }
+
+  function generatePointLights(renderModel, num){
+    let pointLights = [];
+    for(let i = 0; i < num; i++){
+      let pointLight = new PointLight(renderModel, new Vec3f(1.0, 1.0, 1.0), 0.2, 1.0, 1.0,
+                                        new Vec3f(MathExt.randomInteger(-30, 30), MathExt.randomInteger(3, 30), MathExt.randomInteger(-200, -100)),
+                                          1.0, 0.07, 0.014);
+
+      pointLights[i] = pointLight;
+    }
+
+    return pointLights;
   }
 }
 
